86eb955e7d32d95b8b03e85efefed5dd77ed618f
Ciaron Linstead <linstead@pik-potsdam.de>
2018-07-04 09:56:11 +0200

    initial commit

---
+++ part2/src/part2.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : part2.cpp
+// Author      : Ciaron Linstead
+// Version     :
+// Copyright   : (c) 2018
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
