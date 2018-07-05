da67cfa070776daed8b93876d3c595bc7fa7285e
Ciaron Linstead <linstead@pik-potsdam.de>
2018-07-05 15:33:54 +0200

    added Vec3 class. Updated main code.

--- part2/src/part2.cpp
+++ part2/src/part2.cpp
@@ -3,13 +3,25 @@
 // Author      : Ciaron Linstead
 // Version     :
 // Copyright   : (c) 2018
-// Description : Hello World in C++, Ansi-style
+// Description :
 //============================================================================
 
 #include <iostream>
-using namespace std;
+#include "vec3.h"
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+
+	int nx=200;
+	int ny=100;
+	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+	for (int j=ny-1; j>= 0; j--) {
+		for (int i=0; i<nx; i++) {
+			vec3 col(float(i)/float(nx), float(j)/float(ny), 0.2);
+			int ir = int(255.99*col[0]);
+			int ig = int(255.99*col[1]);
+			int ib = int(255.99*col[2]);
+
+			std::cout << ir << " " << ig << " " << ib << "\n";
+		}
+	}
 }
