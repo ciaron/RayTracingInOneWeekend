b604397bc1309d813b87d9647471c0569c63ccac
Ciaron Linstead <ciaron.linstead@gmail.com>
2018-07-19 14:52:54 +0200

    tidy up

--- part3/src/ray.h
+++ part3/src/ray.h
@@ -15,10 +15,8 @@ public:
 
 	ray() {}
 	ray(const vec3& a, const vec3& b) { A = a; B = b; } // ray(origin, direction);
-
 	vec3 origin() const { return A; }
 	vec3 direction() const { return B; }
-
 	vec3 point_at_parameter(float t) const {return A + t*B; }
 
 	vec3 A; // ray origin
