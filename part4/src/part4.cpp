420bd4e3b723a22944ee15846b30699cbf216a63
Ciaron Linstead <ciaron.linstead@gmail.com>
2018-07-19 15:57:30 +0200

    .

--- part4/src/part4.cpp
+++ part4/src/part4.cpp
@@ -1,5 +1,5 @@
 //============================================================================
-// Name        : part4.cpp
+// Name        : part4.cpp - adding a sphere
 // Author      : Ciaron Linstead
 // Version     :
 // Copyright   : (c) 2018
@@ -9,7 +9,20 @@
 #include <iostream>
 #include "ray.h"
 
+bool hit_sphere(const vec3& center, float radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = 2.0 * dot(oc, r.direction());
+    float c = dot(oc, oc) - radius*radius;
+    float discriminant = b*b - 4*a*c;
+    return (discriminant > 0);
+
+}
+
 vec3 color(const ray& r) {
+    if (hit_sphere(vec3(0,0,-1), 0.5, r))
+        return vec3(1,0,0);
+
 	vec3 unit_direction = unit_vector(r.direction());
 	float t = 0.5*(unit_direction.y() + 1.0);
 	// next, a lerp from white (1,1,1) to blue(0.5,0.7,1,0);
